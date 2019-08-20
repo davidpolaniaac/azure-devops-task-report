ae4d0f132a3d04916bcfa7a51264dff9a0207b60
David Polania <davidpolaniaac@users.noreply.github.com>
2019-08-20 13:31:46 -0500

    update APIs and save report in releases

--- publishReport/index.ts
+++ publishReport/index.ts
@@ -1,36 +1,31 @@
-import tl = require('azure-pipelines-task-lib/task');
-import * as common from './common';
+import * as tl from 'azure-pipelines-task-lib/task';
+import * as common from 'azure-devops-task-utils';
 import * as nodeApi from 'azure-devops-node-api';
 import * as GitApi from 'azure-devops-node-api/GitApi';
 import * as GitInterfaces from 'azure-devops-node-api/interfaces/GitInterfaces';
 
 const Inliner = require('inliner');
-const REPOSITORY_NAME: string = "DevOps_Vault_Reports_Extension";
-const RELEASE_DEFINITIONID = tl.getVariable("RELEASE_DEFINITIONID");
-const RELEASE_DEFINITIONENVIRONMENTID = tl.getVariable("RELEASE_DEFINITIONENVIRONMENTID");
-const RELEASE_RELEASEID = tl.getVariable("RELEASE_RELEASEID");
-const RELEASE_ATTEMPTNUMBER = tl.getVariable("RELEASE_ATTEMPTNUMBER");
-const COMMIT_INIT = "0000000000000000000000000000000000000000";
 
 async function createReport(path: string): Promise<any> {
     return new Promise(function (resolve, reject) {
         new Inliner(path, function (error: any, html: any) {
-            resolve(html);
             if (error) {
-                reject(new Error('the report could not be converted'))
+                reject(new Error('the report could not be converted,  validate that the report exists !'))
+            } else {
+                resolve(html);
             }
         });
-      });
+    });
 }
 
-async function createNewRepository(gitApiObject: GitApi.IGitApi, nameRepository : string, project: string ): Promise<GitInterfaces.GitRepository> {
+async function createNewRepository(gitApiObject: GitApi.IGitApi, nameRepository: string, project: string): Promise<GitInterfaces.GitRepository> {
 
     let newRepo: GitInterfaces.GitRepository = <GitInterfaces.GitRepository>{};
 
-    try{
+    try {
 
         common.banner("Create a repository");
-        const createOptions: GitInterfaces.GitRepositoryCreateOptions = <GitInterfaces.GitRepositoryCreateOptions>{name: nameRepository};
+        const createOptions: GitInterfaces.GitRepositoryCreateOptions = <GitInterfaces.GitRepositoryCreateOptions>{ name: nameRepository };
         newRepo = await gitApiObject.createRepository(createOptions, project);
         common.heading(newRepo.name as string);
     }
@@ -44,37 +39,37 @@ async function createNewRepository(gitApiObject: GitApi.IGitApi, nameRepository 
 
 }
 
-async function getNewRepository(gitApiObject: GitApi.IGitApi, nameRepository : string, project: string ): Promise<GitInterfaces.GitRepository> {
+async function getNewRepository(gitApiObject: GitApi.IGitApi, nameRepository: string, project: string): Promise<GitInterfaces.GitRepository> {
 
     let repository: GitInterfaces.GitRepository = <GitInterfaces.GitRepository>{};
 
     common.banner("Get Repository");
 
-    repository = await gitApiObject.getRepository(nameRepository,project);
+    repository = await gitApiObject.getRepository(nameRepository, project);
 
-    if(repository){
+    if (repository) {
 
         common.heading(" ¡ Repository ready !");
 
-    }else{
-        
-        repository = await createNewRepository(gitApiObject,nameRepository,project);
+    } else {
+
+        repository = await createNewRepository(gitApiObject, nameRepository, project);
     }
 
     return repository;
 }
 
-async function getlastestCommit(nameRepository : string, gitApiObject: GitApi.IGitApi, project: string): Promise<string> {
-
+async function getlastestCommit(nameRepository: string, gitApiObject: GitApi.IGitApi, project: string): Promise<string> {
+    const COMMIT_INIT = "0000000000000000000000000000000000000000";
     common.banner("Get lastest Commit");
-    const commitCriteria: GitInterfaces.GitQueryCommitsCriteria = <GitInterfaces.GitQueryCommitsCriteria> {$skip: 0, $top: 1};
-    const commits: GitInterfaces.GitCommitRef[] =  await gitApiObject.getCommits(nameRepository, commitCriteria, project);
-    let lastestCommit:string = COMMIT_INIT;
+    const commitCriteria: GitInterfaces.GitQueryCommitsCriteria = <GitInterfaces.GitQueryCommitsCriteria>{ $skip: 0, $top: 1 };
+    const commits: GitInterfaces.GitCommitRef[] = await gitApiObject.getCommits(nameRepository, commitCriteria, project);
+    let lastestCommit: string = COMMIT_INIT;
 
     if (typeof commits === undefined || commits.length == 0) {
         common.heading("commit init");
-    }else{
-        lastestCommit =  commits[0].commitId as string;
+    } else {
+        lastestCommit = commits[0].commitId as string;
         console.log("Top commit in this repository: ", lastestCommit);
     }
 
@@ -82,36 +77,59 @@ async function getlastestCommit(nameRepository : string, gitApiObject: GitApi.IG
 
 }
 
-function getPush(inputReportName: string, content: string, lastestCommit: string, repository: GitInterfaces.GitRepository): GitInterfaces.GitPush {
+function getPush(inputReportName: string, content: string, lastestCommit: string, repository: GitInterfaces.GitRepository, reportNameRandom: string): GitInterfaces.GitPush {
 
-    const push : GitInterfaces.GitPush = <GitInterfaces.GitPush>{
+    const RELEASE_DEFINITIONID = tl.getVariable("RELEASE_DEFINITIONID");
+    const RELEASE_DEFINITIONENVIRONMENTID = tl.getVariable("RELEASE_DEFINITIONENVIRONMENTID");
+    const RELEASE_RELEASEID = tl.getVariable("RELEASE_RELEASEID");
+    const RELEASE_ATTEMPTNUMBER = tl.getVariable("RELEASE_ATTEMPTNUMBER");
+
+    const push: GitInterfaces.GitPush = <GitInterfaces.GitPush>{
         commits: [
-        {
-            comment: inputReportName,
-            changes: [
-                {
-                    changeType: GitInterfaces.VersionControlChangeType.Add,
-                    item: {
-                        path: `/${RELEASE_DEFINITIONID}/${RELEASE_RELEASEID}/${RELEASE_DEFINITIONENVIRONMENTID}/${RELEASE_ATTEMPTNUMBER}/${Date.now()}.html`,
-                    },
-                    newContent: {
-                        content: content,
-                        contentType: GitInterfaces.ItemContentType.RawText
+            {
+                comment: inputReportName,
+                changes: [
+                    {
+                        changeType: GitInterfaces.VersionControlChangeType.Add,
+                        item: {
+                            path: `/${RELEASE_DEFINITIONID}/${RELEASE_RELEASEID}/${RELEASE_DEFINITIONENVIRONMENTID}/${RELEASE_ATTEMPTNUMBER}/${reportNameRandom}.html`,
+                        },
+                        newContent: {
+                            content: content,
+                            contentType: GitInterfaces.ItemContentType.RawText
+                        }
                     }
-                }
-            ]
-        }
-    ],refUpdates:[
-        {
-            name: "refs/heads/master",
-            oldObjectId: lastestCommit
-        }
-    ], repository };
+                ]
+            }
+        ], refUpdates: [
+            {
+                name: "refs/heads/master",
+                oldObjectId: lastestCommit
+            }
+        ], repository
+    };
 
     return push;
 }
 
-async function saveReport (content: string, nameRepository : string, inputReportName : string) {
+async function saveReportInRelease(html: string, reportNameRandom: string) {
+
+    try {
+
+        common.banner("Save a report in release");
+        const SYSTEM_DEFAULTWORKINGDIRECTORY = tl.getVariable("SYSTEM_DEFAULTWORKINGDIRECTORY");
+        tl.writeFile(`${SYSTEM_DEFAULTWORKINGDIRECTORY}/${reportNameRandom}.html`, html, 'utf8');
+        tl.addAttachment('publish-report', reportNameRandom, `${SYSTEM_DEFAULTWORKINGDIRECTORY}/${reportNameRandom}.html`);
+        common.heading("¡ report has been saved successfully in release !");
+
+    } catch (err) {
+
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+
+}
+
+async function saveReportInRepository(content: string, nameRepository: string, inputReportName: string, reportNameRandom: string) {
 
     common.banner('Git Configuration');
 
@@ -122,19 +140,19 @@ async function saveReport (content: string, nameRepository : string, inputReport
     common.heading('Project');
     common.heading(project);
 
-    const repository: GitInterfaces.GitRepository = await getNewRepository(gitApiObject,nameRepository,project);
-    const lastestCommit : string  = await getlastestCommit(nameRepository, gitApiObject, project);
+    const repository: GitInterfaces.GitRepository = await getNewRepository(gitApiObject, nameRepository, project);
+    const lastestCommit: string = await getlastestCommit(nameRepository, gitApiObject, project);
 
-    try{
+    try {
 
-        common.banner("Save a report");
-        const push : GitInterfaces.GitPush = getPush(inputReportName, content, lastestCommit, repository);
-        await gitApiObject.createPush(push,repository.id as string,project);
-        common.heading("¡ report has been saved successfully !");
+        common.banner("Save a report in repository");
+        const push: GitInterfaces.GitPush = getPush(inputReportName, content, lastestCommit, repository, reportNameRandom);
+        await gitApiObject.createPush(push, repository.id as string, project);
+        common.heading("¡ report has been saved successfully in repository !");
+
     }
     catch (err) {
 
-        console.log(err.message);
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 
@@ -143,12 +161,18 @@ async function saveReport (content: string, nameRepository : string, inputReport
 async function run() {
 
     try {
+
+        const REPOSITORY_NAME: string = "DevOps_Vault_Reports_Extension";
+        const reportNameRandom: string = String(Date.now());
         const inputPathReport: string = tl.getInput('htmlPath', true);
         const inputReportName: string = tl.getInput('nameRepository', true);
         const html = await createReport(inputPathReport);
-        await saveReport(html,REPOSITORY_NAME,inputReportName);
+        await saveReportInRelease(html, reportNameRandom);
+        await saveReportInRepository(html, REPOSITORY_NAME, inputReportName, reportNameRandom);
+        
     }
     catch (err) {
+
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
 }
